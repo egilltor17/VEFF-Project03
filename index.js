44428544c981d947c9966378be9df6854265b0c2
egilltor17 <egilltor17@ru.is>
2019-03-16 20:37:00 +0000

    refactored validation functions

--- index.js
+++ index.js
@@ -38,13 +38,15 @@ var observations = [
 
 var errorMessages = [
     "All good.",
-    "There are undefined values in the json body.",
-    "Latitude must be in range [-90, 90].",
-    "Longitude must be in range [-180, 180].",
-    "Wind speed must not be negative.",
-    "Wind direction must be lowercase cardinal",
-    "Precipitation must not be negative.",
-    "Humidity muts be in range [0, 100]"
+    "The request body is undefined.",
+    "Description \"description:\" is missing",
+    "Latitude \"lat:\" must be in range [-90, 90].",
+    "Longitude \"lon:\" must be in range [-180, 180].",
+    "Tempeture \"temp:\" must be a number",
+    "Wind speed \"windSpeed:\" must not be negative.",
+    "Wind direction \"windDir:\" must be lowercase cardinal direction",
+    "Precipitation \"prec:\" must not be negative.",
+    "Humidity \"hum:\" musts be in range [0, 100]"
 ];
 
 /* ============================================================================================ */
